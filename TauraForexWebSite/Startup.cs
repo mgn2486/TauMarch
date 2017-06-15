2b7ed45831ca50c33b858ee422cce13d117d843e
massy <massy19@gmail.com>
2017-06-15 03:39:39 +0200

    Add project files.

---
+++ TauraForexWebSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TauraForexWebSite.Startup))]
+namespace TauraForexWebSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
