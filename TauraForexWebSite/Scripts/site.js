730a4df49546a05d056bb09ac04874cd3be50916
Masimba Gomba Ndadzibaya <masimban@sahomeloans.com>
2017-12-15 12:51:06 +0200

    Updated the random text to be displayaed on the screen

---
+++ TauraForexWebSite/Scripts/site.js
@@ -0,0 +1,12 @@
+﻿var text = ["Welcome", "To", "Train to Trade"];
+var counter = 0;
+var elem = document.getElementById("motivationalText");
+setInterval(change, 1000);
+
+function change() {
+    elem.innerHTML = text[counter];
+    counter++;
+    if (counter >= text.length) {
+        counter = 0;
+    }
+}
