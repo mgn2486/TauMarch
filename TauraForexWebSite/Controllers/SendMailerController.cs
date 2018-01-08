63a481714201a7b3439f0bfb62bbf6a5ccb89f18
Masimba Gomba Ndadzibaya <masimban@sahomeloans.com>
2018-01-08 07:42:57 +0200

    Updated the mail sending files

---
+++ TauraForexWebSite/Controllers/SendMailerController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TauraForexWebSite.Controllers
+{
+	public class SendMailerController
+	{
+	}
+}
